f3d2b26638d63ca2eb5a3b265613932cba0d4072
angelolad <angelo.ladores@outlook.com>
2023-02-18 16:32:55 -0500

    Add bcyrpt auth

--- 2_ruby_on_rails/3_rails/practice/micro-reddit2/app/controllers/sessions_controller.rb
+++ 2_ruby_on_rails/3_rails/practice/micro-reddit2/app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:session][:username].downcase)
-    if user && user = User.find_by(password: params[:session][:password])
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
       #set session and redirect on sucess
       log_in user
       redirect_to root_path
     else
       #error message on fail
       flash[:danger] = "Invalid email/password combination"
-      redirect_to login_path
+      redirect_to login_path, status: :unprocessable_entity
     end
   end
 
--- 2_ruby_on_rails/3_rails/practice/micro-reddit2/app/models/user.rb
+++ 2_ruby_on_rails/3_rails/practice/micro-reddit2/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_secure_password
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true
