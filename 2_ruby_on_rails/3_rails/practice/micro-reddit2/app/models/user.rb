7b74c32da474ed60fc4c88c524b8fe9e15f80e24
angelolad <angelo.ladores@outlook.com>
2023-02-18 19:42:23 -0500

    add devise to exisiting user model

--- 2_ruby_on_rails/3_rails/practice/micro-reddit2/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_many :posts
-  has_secure_password
-  validates :username, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-end
