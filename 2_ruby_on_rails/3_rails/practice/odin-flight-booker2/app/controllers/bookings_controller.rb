a9867bc751e92cbed2697f8196e37f999fc8fac6
angelolad <angelo.ladores@outlook.com>
2023-02-28 16:05:18 -0500

    add ui and bookings tab

--- 2_ruby_on_rails/3_rails/practice/odin-flight-booker2/app/controllers/bookings_controller.rb
+++ 2_ruby_on_rails/3_rails/practice/odin-flight-booker2/app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
@@ -11,9 +13,7 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.user_id = current_user.id
-    puts "*************************"
-    puts @booking.inspect
-    puts "*************************"
+
     if @booking.save
       redirect_to booking_path(@booking)
     else
@@ -22,7 +22,20 @@ class BookingsController < ApplicationController
   end
 
   def show
-    @booking = Booking.find(params[:id])
+    @booking_show = Booking.find(params[:id])
+    if current_user.id == @booking_show.user_id
+      @flight_show = Flight.find(@booking_show.flight_id)
+      @passengers_show = Passenger.where(booking_id: @booking_show.id)
+    else
+      flash[:notice] = "Invalid user cannot access requested booking."
+      redirect_to root_path
+    end
+  end
+
+  def index
+    @booking_index = Booking.where(user_id: current_user.id)
+    @passengers_index = Passenger.where(booking_id: @booking_index.pluck(:id))
+    @flight_index = Flight.where(id: @booking_index.pluck(:flight_id))
   end
 
   private
