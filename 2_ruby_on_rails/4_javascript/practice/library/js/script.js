5189de033173debde70ed6587404164544dab4e3
angelolad <angelo.ladores@outlook.com>
2023-03-28 19:36:33 -0400

    initial commit

--- 2_ruby_on_rails/4_javascript/practice/library/js/script.js
+++ 2_ruby_on_rails/4_javascript/practice/library/js/script.js
@@ -1,111 +1,97 @@
-let myLibrary = [];
-let bookForm = document.getElementById("book_form");
-let libraryRow = document.getElementById("library_row");
-/*let bookCards = document.querySelectorAll('.book_card').forEach(item => {
-  item.addEventListener('click', event => {
-    console.log("hello");
-  })
-});*/
-function Book(id, title, author, pages, read) {
-  this.id = id;
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-function addBookToLibrary() {
-
-  let title = document.getElementById("title");
-  let author = document.getElementById("author");
-  let pages = document.getElementById("pages");
-  let read = document.getElementById("read");
-
-  if (title.value == "" || author.value == "" || pages.value == "") {
-    console.log("Empty form");
+class Book {
+  constructor(id, title, author, pages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
-  else {
 
-    let currentId = getCurrentElementId();
-    let book = new Book(currentId, title.value, author.value, pages.value, read.checked);
-    myLibrary.push(book);
-    console.log(myLibrary);
-    displayLibrary(currentId);
+  get readValue() {
+    return this.read ? "Yes" : "No";
+  }
 
+  toggleRead() {
+    this.read = !this.read;
   }
 }
 
-function getCurrentElementId() {
-  if (myLibrary.length == 0) {
-    return 0;
+class Library {
+  constructor() {
+    this.books = [];
+    this.bookForm = document.getElementById("book_form");
+    this.libraryRow = document.getElementById("library_row");
+    this.currentId = 0;
+    this.setStaticListeners();
   }
-  else {
-    var lastId = myLibrary[myLibrary.length - 1]["id"];
 
-    return lastId + 1;
-  }
-}
+  addBookToLibrary() {
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const pages = document.getElementById("pages");
+    const read = document.getElementById("read");
 
-function displayLibrary(id) {
-
-  let bookCard = createDomElements(id);
-  libraryRow.appendChild(bookCard);
-
-}
-
-function createDomElements(id) {
-  let { title, author, pages, read, id: bookId } = myLibrary[myLibrary.length - 1];
-
-  let bookCard = document.createElement('div');
-  bookCard.setAttribute('id', `book_${id}`)
-  bookCard.classList.add("book_card");
-  let readValue = read ? "Yes" : "No";
-  bookCard.innerHTML = `
-    <h1>ID: ${bookId}</h1>
-    <h2>Title: ${title}</h2>
-    <p>Author: ${author}</p>
-    <p>Pages: ${pages}</p> 
-    <p id="read_${bookId}">Read: ${readValue}</p>      
-    <button id="change_${bookId}">Change Status</button>
-    <button id="delete_${bookId}">Delete Book</button>
-    `
-  bookCard.querySelector(`#delete_${bookId}`).addEventListener('click', function () {
-    bookCard.remove();
-
-    let index = myLibrary.indexOf(myLibrary[id]);
-    myLibrary.splice(index, 1);
-    console.log(myLibrary);
-
-    //console.log(x);
-  });
-
-  bookCard.querySelector(`#change_${bookId}`).addEventListener('click', function () {
-
-    let index = myLibrary.indexOf(myLibrary[id]);
-    let readValue = document.getElementById(`read_${bookId}`);
-    if (myLibrary[index]["read"] == false) {
-      myLibrary[index]["read"] = true
-      readValue.innerHTML = "Read: Yes";
+    if (title.value == "" || author.value == "" || pages.value == "") {
+      console.log("Empty form");
     }
     else {
-      myLibrary[index]["read"] = false;
-      readValue.innerHTML = "Read: No";
+      const book = new Book(this.currentId, title.value, author.value, pages.value, read.checked);
+      this.books.push(book);
+      console.log(this.books);
+      this.displayBook(book);
+      this.currentId++;
     }
+  }
+  displayBook(book) {
+    const bookCard = this.createDomElements(book);
+    this.libraryRow.appendChild(bookCard);
+  }
 
-    //console.log(x);
-  });
+  createDomElements(book) {
+    //let { title, author, pages, read, id: bookId } = myLibrary[myLibrary.length - 1];
 
-  return bookCard;
+    let bookCard = document.createElement('div');
+    bookCard.setAttribute('id', `book_${book.id}`)
+    bookCard.classList.add("book_card");
+    bookCard.innerHTML = `
+      <h1>ID: ${book.id}</h1>
+      <h2>Title: ${book.title}</h2>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p> 
+      <p id="read_${book.id}">Read: ${book.readValue}</p>      
+      <button id="change_${book.id}">Change Status</button>
+      <button id="delete_${book.id}">Delete Book</button>
+      `
+    bookCard.querySelector(`#delete_${book.id}`).addEventListener('click', function () {
+
+      this.deleteBook(book);
+      bookCard.remove();
+
+      console.log(this.books);
+    }.bind(this));
+
+    bookCard.querySelector(`#change_${book.id}`).addEventListener('click', function () {
+      book.toggleRead();
+      const readValue = document.getElementById(`read_${book.id}`);
+      readValue.innerHTML = `Read: ${book.readValue}`;
+    });
+
+    return bookCard;
+  }
+  deleteBook = (book) => {
+    const index = this.books.indexOf(book);
+    this.books.splice(index, 1);
+  }
+
+  setStaticListeners() {
+    this.bookForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.addBookToLibrary();
+    });
+  }
+
 }
+const library = new Library();
 
-function setStaticListeners() {
-  bookForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addBookToLibrary();
-  });
-}
 
-function init() {
-  setStaticListeners();
-}
-init();
+
