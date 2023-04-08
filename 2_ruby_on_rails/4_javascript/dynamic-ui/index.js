1ce15d17de49f28186263a48e4fd5d5c608e792e
angelolad <angelo.ladores@outlook.com>
2023-04-08 17:04:46 -0400

    complete

--- 2_ruby_on_rails/4_javascript/dynamic-ui/index.js
+++ 2_ruby_on_rails/4_javascript/dynamic-ui/index.js
@@ -1,1 +1,58 @@
-console.log("Hello");
+function dropDown() {
+  const dropDownMenu = document.getElementById("dropdown-menu");
+  dropDownMenu.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropdown-button')) {
+    const dropDownMenu = document.getElementById("dropdown-menu");
+    if (dropDownMenu.classList.contains("show")) {
+      dropDownMenu.classList.remove("show");
+    }
+  }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeId(move) {
+  showSlides(slideIndex += move);
+}
+/* function moveSlideShow(slideIndex) {
+
+  console.log(slideIndex);
+  const images = document.getElementsByClassName("slide-card");
+
+  if (slideIndex > images.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = images.length;
+  }
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove("show-slide");
+    images[i].classList.add("hide");
+  }
+  images[slideIndex - 1].classList.remove("hide");
+  images[slideIndex - 1].classList.add("show-slide");
+} */
+
+function showSlides(n) {
+  let i;
+  const images = document.getElementsByClassName("slide-card");
+
+  console.log(slideIndex);
+  if (n > images.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = images.length;
+  }
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+
+  images[slideIndex - 1].style.display = "block";
+}
